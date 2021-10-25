e2d9ddad92ad871a2b2b97843852ee4e14bc941c
Absa KEBE <SARR028856@orange-sonatel.com>
2021-10-25 19:50:31 +0000

    exoliste[:2]2.py

---
+++ day7/exo2.py
@@ -0,0 +1,7 @@
+liste=["Mer", "Sandaga", "Terre", "Thiaroye", "Yoff", "Sur", "Almadies"]
+resultat=liste[3] + "-" + liste[5] + "-" + liste[0]
+print(resultat)
+first=liste[:2]
+print(first)
+last=liste[-2:]
+print(last)
