068cc43f0ff978d247a320b9137a3c7f4d1a7486
Absa KEBE <SARR028856@orange-sonatel.com>
2021-10-28 15:57:46 +0000

    exo1.py

---
+++ day10/exo1.py
@@ -0,0 +1,9 @@
+dico = {
+        "nombre" : 2,
+        "lieu": "Senegal",
+        "Magal" : ["Touba", "Porokhane", "Kazu Rajab"],
+        "Gamou" : ["Tivaouane","Touba","Medina","Ndiassane"]
+        }
+for key in dico:
+    print(dico)
+    break
---
+++ day10/exo2.py
@@ -0,0 +1,4 @@
+dico =dict()
+for x in range(1,16):
+    dico[x] = x ** 2
+print(dico)
