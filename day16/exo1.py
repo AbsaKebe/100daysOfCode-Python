89c0cb93d429ab39f76511d18463feb38b9dbcd6
Absa KEBE <SARR028856@orange-sonatel.com>
2021-11-04 21:15:31 +0000

    Delta Dayzz

--- day16/exo1.py
+++ day16/exo1.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-from datetime import timedelta
 date_1 = datetime(2021, 10, 20)
-print(date_1)
 date_2 = datetime(2021, 11, 4)
-print(date_2)
 nbofdays=None
+
 if date_2>date_1:
     nbofdays=date_2 - date_1
 else:
     nbofdays=date_1 - date_2
+    
 print(nbofdays.days)
