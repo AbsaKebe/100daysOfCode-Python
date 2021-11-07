1faaef90abbbc59514e609ae5487634e68719c27
Absa KEBE <SARR028856@orange-sonatel.com>
2021-11-07 19:52:04 +0000

    Groupby & max

--- day18/exo2.py
+++ day18/exo2.py
@@ -1,4 +1,6 @@
 import pandas as pd
 auto = pd.read_csv("Automobile_data.csv")
-auto = auto.sort_values(by=['price'], ascending=False)
-print(auto["company" ,"price"])
+#auto = auto.max(['company']).get_group('volvo')
+auto = auto.groupby(['company'])
+auto = auto['price'].max()
+print(auto)
