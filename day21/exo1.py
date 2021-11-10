ca5738bce1024e16441e1f03554bc8cdb0c0e840
Absa KEBE <SARR028856@orange-sonatel.com>
2021-11-10 22:44:39 +0000

    Sheet Name

--- day21/exo1.py
+++ day21/exo1.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 #Sélectionner le sheet name parameter details via index
 inco=pd.read_excel("PCE_huawei_GSM_OSN_NADC_211109092550.xlsx", sheet_name=1, usecols="H,J", dtype="int")
+#Sélectionner les colonnes A,H J du sheet name Parameter details 
 inco=pd.read_excel("PCE_huawei_GSM_OSN_NADC_211109092550.xlsx", sheet_name=1, usecols="A,H,J")
 print(inco)
 #inco=pd.read_excel("PCE_huawei_GSM_OSN_NADC_211109092550.xlsx", sheet_name="Parameter Details")
-#Sélectionner les colonnes A,H J du sheet name Parameter details 
+
